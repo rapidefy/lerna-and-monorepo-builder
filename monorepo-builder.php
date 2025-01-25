b5b94f30f9477b765ec866fa5a4a868ec66de9a0
Mamadou hady barry <seidya.pro.mhb@gmail.com>
2025-01-25 12:14:37 +0000

    prepare release

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -2,8 +2,13 @@
 
 declare(strict_types=1);
 
+use MonorepoBuilderPrefix202408\Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\MonorepoBuilder\Config\MBConfig;
 
-return static function (MBConfig $mbConfig): void {
+return static function (MBConfig $mbConfig, ContainerConfigurator $containerConfigurator): void {
     $mbConfig->packageDirectories([__DIR__ . '/packages/laravel']);
+
+
+
+
 };
