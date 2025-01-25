658a7f570c222b574601b115b21afe187cdc9d46
Mamadou hady barry <seidya.pro.mhb@gmail.com>
2025-01-25 15:07:35 +0000

    configure split monorepo

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -4,11 +4,25 @@ declare(strict_types=1);
 
 use MonorepoBuilderPrefix202408\Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\MonorepoBuilder\Config\MBConfig;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
 
 return static function (MBConfig $mbConfig, ContainerConfigurator $containerConfigurator): void {
-    $mbConfig->packageDirectories([__DIR__ . '/packages/laravel']);
+  $services = $containerConfigurator->services();
+  $mbConfig->packageDirectories([__DIR__ . '/packages/laravel']);
 
 
-
+  $services->set(UpdateReplaceReleaseWorker::class);
+  $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
+  $services->set(TagVersionReleaseWorker::class);
+  $services->set(PushTagReleaseWorker::class);
+  $services->set(SetNextMutualDependenciesReleaseWorker::class);
+  $services->set(UpdateBranchAliasReleaseWorker::class);
+  $services->set(PushNextDevReleaseWorker::class);
 
 };
