d5fe8b834a56aeffd1277d807f8baac1582f1200
Mamadou hady barry <seidya.pro.mhb@gmail.com>
2025-01-31 12:39:33 +0000

    refactor(forms): update placeholder text in RDate input
    Updated the placeholder text for the RDate input from "R Date" to "Date field" to improve clarity and consistency. This change ensures a more user-friendly label for the input field.

--- packages/vue/forms/src/components/RDate/index.vue
+++ packages/vue/forms/src/components/RDate/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <h1>RDate</h1>
-    <input type="date" placeholder="R Date" name="date1" data-id="date">
+    <input type="date" placeholder="Date field" name="date1" data-id="date">
   </div>
 </template>
