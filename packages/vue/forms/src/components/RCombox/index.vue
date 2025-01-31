bd120a892d638cb33662215bb59a1829a0175c83
Mamadou hady barry <seidya.pro.mhb@gmail.com>
2025-01-31 14:19:06 +0000

    fix(forms): correct input type in RCombobox component
    Change the input type from 'text' to 'r-combobox' in the RCombobox component. This ensures proper behavior and aligns with the expected functionality of the component.

--- packages/vue/forms/src/components/RCombox/index.vue
+++ packages/vue/forms/src/components/RCombox/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>RCombobox</h1>
-    <input type="text" placeholder="select">
+    <input type="r-combobox" placeholder="select">
   </div>
 </template>
 <script setup lang="ts">
