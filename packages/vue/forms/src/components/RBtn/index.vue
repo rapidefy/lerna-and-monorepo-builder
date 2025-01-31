f60a7f2658f2ff965d8d17fc370f2ddc4738bc46
Mamadou hady barry <seidya.pro.mhb@gmail.com>
2025-01-31 15:38:22 +0000

    feat(forms): add props to RBtn component
    Introduced a "text" prop to the RBtn component, making it more versatile and customizable. The prop is required and ensures the button displays dynamic content based on passed input.

--- packages/vue/forms/src/components/RBtn/index.vue
+++ packages/vue/forms/src/components/RBtn/index.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
-
+import { defineProps } from 'vue'
+const props = defineProps({
+  text: {
+    type: String,
+    default: () => null,
+    required: true,
+  }
+})
 </script>
 
 <template>
   <div>
-    <v-btn>RBtn</v-btn>
+    <v-btn>{{ text }}</v-btn>
   </div>
 </template>
 
